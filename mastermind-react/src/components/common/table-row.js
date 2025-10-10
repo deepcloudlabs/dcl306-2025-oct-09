6142a3edb630cd5bff9e7e645c041e90fa5fa7cf
Binnur KURT <deepcloudlabs@gmail.com>
2025-10-10 14:17:08 +0300

    Update table-row.js

--- mastermind-react/src/components/common/table-row.js
+++ mastermind-react/src/components/common/table-row.js
@@ -5,7 +5,7 @@ export default function TableRow({rowkey,value,fields}) {
         <tr key={rowkey}>
             {
                 fields.map( (field,field_index) =>
-                    (<TableData cellkey={field_index} value={value[field]} />)
+                    (<TableData key={field_index} cellkey={field_index} value={value[field]} />)
                 )
             }
         </tr>
