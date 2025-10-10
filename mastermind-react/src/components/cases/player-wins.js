40937a9d76633b096403e8f23de5b29e580a15ff
Binnur KURT <deepcloudlabs@gmail.com>
2025-10-10 14:19:36 +0300

    Update player-wins.js

--- mastermind-react/src/components/cases/player-wins.js
+++ mastermind-react/src/components/cases/player-wins.js
@@ -1,11 +1,12 @@
 import Container from "../common/container";
 import Card from "../common/card";
+import {Link} from "react-router";
 
 export default function PlayerWins({}){
     return (
         <Container>
-            <Card title={"Congrats"}>
-                You win!
+            <Card title={"Congrats: You win!"}>
+                <Link to={"/"}>Play again?</Link>
             </Card>
         </Container>
     )
