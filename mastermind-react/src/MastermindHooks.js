24b7afd705d7f196dca69a1b9bf60f12326fc65d
Binnur KURT <deepcloudlabs@gmail.com>
2025-10-10 12:16:03 +0300

    Update MastermindHooks.js

--- mastermind-react/src/MastermindHooks.js
+++ mastermind-react/src/MastermindHooks.js
@@ -7,25 +7,37 @@ import Table from "./components/common/table";
 import React, {useEffect, useState} from "react";
 import create_secret, {evaluateMove} from "./utils/game-utils";
 import {useNavigate} from "react-router";
+
 const initial_secret = create_secret(3);
-export default function MastermindHooks(){
-    const [secret,setSecret] = useState(initial_secret);
-    const [level,setLevel] = useState(3);
-    const [lives,setLives] = useState(3);
-    const [tries,setTries] = useState(0);
-    const [counter,setCounter] = useState(100);
-    const [guess,setGuess] = useState(123);
-    const [moves,setMoves] = useState([]);
-    const [constraints,setConstraints] = useState({
+export default function MastermindHooks() {
+    const [secret, setSecret] = useState(initial_secret);
+    const [level, setLevel] = useState(3);
+    const [lives, setLives] = useState(3);
+    const [tries, setTries] = useState(0);
+    const [counter, setCounter] = useState(100);
+    const [guess, setGuess] = useState(123);
+    const [moves, setMoves] = useState([]);
+    const [constraints, setConstraints] = useState({
         max_tries: 10,
         timeout: 100
     });
 
     const navigate = useNavigate();
 
-    useEffect(()=>{
+    useEffect(() => {
         const timer_id = setInterval(() => {
-            setCounter( prevCounter => prevCounter - 1);
+            setCounter(prevCounter => prevCounter - 1);
+            if (counter <= 0) {
+                setLives(prevLives => prevLives - 1);
+                if (lives <= 0) {
+                    navigate("/loses");
+                    return;
+                }
+                setSecret(create_secret(level));
+                setMoves([]);
+                setTries(0);
+                setCounter(constraints.timeout);
+            }
         }, 1_000);
         return () => {
             clearInterval(timer_id);
@@ -50,19 +62,19 @@ export default function MastermindHooks(){
                 navigate("/wins");
                 return;
             }
-            setSecret(create_secret(level+1));
+            setSecret(create_secret(level + 1));
             setMoves([]);
             setTries(0); // schedules state updates!
             // bulk/batch updates => performance
             setConstraints(prevContraints => ({
                 max_tries: prevContraints.max_tries + 2,
-                timeout: prevContraints.timeout+40
+                timeout: prevContraints.timeout + 40
             }));
-            setCounter(constraints.timeout+40);
+            setCounter(constraints.timeout + 40);
             setLives(prevLives => prevLives + 1);
         } else {
             if (tries > constraints.max_tries) {
-                if(lives === 0){
+                if (lives === 0) {
                     //player loses! -> routing
                     navigate("/loses");
                     return;
@@ -72,12 +84,12 @@ export default function MastermindHooks(){
                 setMoves([]);
                 setTries(0);
             } else {
-                setMoves([...moves,evaluateMove(guess, secret)]);
+                setMoves([...moves, evaluateMove(guess, secret)]);
             }
         }
     }
 
-    return(
+    return (
         <Container>
             <p></p>
             <Card title={"Mastermind Game Console"}>
@@ -94,7 +106,8 @@ export default function MastermindHooks(){
             </Card>
             <p></p>
             <Card title={"Moves"}>
-                <Table column_names={["Guess","Perfect Match","Partial Match","Message"]} items={moves} fields={["guess","perfect_match","partial_match","message"]}></Table>
+                <Table column_names={["Guess", "Perfect Match", "Partial Match", "Message"]} items={moves}
+                       fields={["guess", "perfect_match", "partial_match", "message"]}></Table>
             </Card>
-        </Container>    );
+        </Container>);
 }
